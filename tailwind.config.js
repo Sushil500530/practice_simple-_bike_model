d0ee0b4d1f7d0b2133ab321151a98994d3db58c6
Sushil97 <sushil500530@gmail.com>
2023-07-24 11:09:59 +0600

    practice bike model and rating other

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {
+//       color:{
+//         'bike-primary':'#E76F51' ,
+//         "bg-bike-color":'#264653' ,
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
